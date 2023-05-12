bddff846567dd98edfde45fc3049b7b582a0072c
Your Name <otutaofeeqi@gmail.com>
2023-05-12 16:50:21 +0100

    FODS first commit

--- FODS/settings.py
+++ FODS/settings.py
@@ -83,7 +83,7 @@ ELASTICSEARCH_DSL = {
 }
 
 
-CSRF_TRUSTED_ORIGINS = ['https://web-production-0d7a.up.railway.app/']
+CSRF_TRUSTED_ORIGINS = ['*']
 
 ELASTICSEARCH_INDEX_NAMES = {
     'Orders.Food': 'search_index',
