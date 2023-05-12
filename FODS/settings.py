0f50c252fa71cbda6a6bde096fa9c94f2f4fafc8
Your Name <otutaofeeqi@gmail.com>
2023-05-12 16:40:09 +0100

    FODS first commit

--- FODS/settings.py
+++ FODS/settings.py
@@ -28,7 +28,7 @@ DATABASE_URL = os.getenv('DATABASE_URL')
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
