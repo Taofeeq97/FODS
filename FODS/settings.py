56271ef99ddb89b58762ecaac12250a44b4c5060
Your Name <otutaofeeqi@gmail.com>
2023-05-17 16:39:25 +0100

    FODS first commit

--- FODS/settings.py
+++ FODS/settings.py
@@ -152,17 +152,17 @@ WSGI_APPLICATION = 'FODS.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=1800)
 }
 
-# DATABASES = {
-#     'default': dj_database_url.config(default=DATABASE_URL, conn_max_age=1800)
-# }
-#
 
 
 # Password validation
